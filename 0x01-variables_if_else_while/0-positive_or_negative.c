c6aa7280c302bf101656dac7823d063143b7506e
Akinbode-Temitope <temmyt2010@gmail.com>
2023-06-19 17:37:44 -0700

    postive numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* Positive numbers*/
+/**
+ * main - prints if a nuber is positive
+ * Returns: Always (success)
+ */
+
 int main(void)
 {
 	int n;
 	srand(time(0));
-	n = rand() % RAND_Max;
-	if (n > o)
-	{	
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
-		printf("%d is Zero\n", n);
+	printf("%d is positive\n", n);
+	}
+	else if (n ==- 0)
+	{
+	printf("%d is Zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
-	}  
+	printf("%d is negative\n", n);
+	}
 	return (0);
-}
 
+}
