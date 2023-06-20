5bac3f7e76482f0a56706411da87870f51ee63ec
Akinbode-Temitope <temmyt2010@gmail.com>
2023-06-19 21:08:38 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints base 10 numbers
+ * Return: Always (Succcess)
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	printf("\n");
+	i++;
+	}
+
+	return (0);
+
+}
