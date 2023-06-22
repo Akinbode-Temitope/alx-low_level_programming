2260b24a709031f79a94901ecde81e427ff2fb09
Akinbode-Temitope <temmyt2010@gmail.com>
2023-06-22 09:22:22 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Debug function that prints positive or negative value
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
